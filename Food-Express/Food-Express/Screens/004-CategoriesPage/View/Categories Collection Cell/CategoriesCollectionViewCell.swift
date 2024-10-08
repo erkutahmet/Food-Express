80a4f97233facf39671e79dc01be4355df5e395d
erkutahmet <90329328+erkutahmet@users.noreply.github.com>
2024-10-08 20:05:18 +0300

    Category Screen
    Category ui setted, Imageviewblur extension added

---
+++ Food-Express/Food-Express/Screens/004-CategoriesPage/View/Categories Collection Cell/CategoriesCollectionViewCell.swift
@@ -0,0 +1,20 @@
+//
+//  CategoriesCollectionViewCell.swift
+//  Food-Express
+//
+//  Created by erkut on 8.10.2024.
+//
+
+import UIKit
+
+class CategoriesCollectionViewCell: UICollectionViewCell {
+
+    @IBOutlet weak var categoryImageView: UIImageView!
+    @IBOutlet weak var categoryNameLbl: UILabel!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
